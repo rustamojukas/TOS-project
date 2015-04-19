23af72caa3f252d8f3cf841770a07976f69631e8
Rustam Ojukas <rustam.ojukas@gmail.com>
2015-04-19 22:08:38 +0300

    python_version/tos.py password version

--- python_version/tos.py
+++ python_version/tos.py
@@ -1,1 +1,28 @@
-__author__ = 'rustam'
+#!/usr/bin/python3
+
+#######################################################################
+# Name: tos.sh                                                        #
+# Description: Script for task over SSH. Required paramiko library    #
+# http://www.paramiko.org/                                            #
+# Author: Rustam Ojukas                                               #
+# Github: https://github.com/rustamojukas/TOS-project                 #
+#######################################################################
+
+# imports
+import paramiko
+
+
+host = '192.168.56.10'
+user = 'test'
+passwd = 'asdf'
+port = 22
+
+tos = paramiko.SSHClient()
+tos.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+tos.connect(hostname=host, username=user, password=passwd, port=port)
+stdin, stdout, stderr = tos.exec_command('ls -la')
+data = stdout.readlines()
+tos.close()
+
+for line in data:
+    print(line)
